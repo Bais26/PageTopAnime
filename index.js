dd873872ba9e26d6b4e36bd0e47ac46cbbe67929
Bais26 <baisyufan2004@gmail.com>
2023-12-19 20:48:51 +0700

    delete nav

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-// fetch('https://api.jikan.moe/v4/top/anime')
-// .then(response => response.json())
-// .then(json => json.data.map(data => {
-//     console.log(data.title);
-// }))
-
-//   const animeList = document.getElementById("listAnime");
-
-//   fetch('https://api.jikan.moe/v4/top/anime')
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error('Network response was not ok');
-//       }
-//       return response.json();
-//     })
-//     .then(data => {
-//       // Memproses data yang diterima dari API
-//       const topAnime = data.data; // Sesuaikan dengan struktur data API
-
-//       // Menampilkan data anime dalam elemen HTML
-//       topAnime.forEach(anime => {
-//         const animeElement = document.createElement('section');
-//         animeElement.innerHTML = `
-//           <img src="${anime.images.jpg.image_url}" alt="${anime.title} Poster">
-//           <h2>${anime.title}</h2>
-//         `;
-//         animeList.appendChild(animeElement);
-//       });
-//     })
-//     .catch(error => {
-//       console.error('There was a problem fetching the data:', error);
-//     });
 const anime = document.getElementById('listAnime');
 let tamplate = document.getElementById('data');
 
@@ -40,14 +8,16 @@ fetch('https://api.jikan.moe/v4/top/anime')
           }
           return response.json();
         })
-.then(data => {
-    const dataAnime = data.data;
+.then(res => {
+    const dataAnime = res.data;
 
     dataAnime.forEach(Anime => {
-    const getData = document.importNode(tamplate.content, true);
+      const { images, title } = Anime;
+      const { jpg } = images;
+      const getData = document.importNode(tamplate.content, true);
 
-    getData.querySelector('img').src = Anime.images.jpg.image_url;
-    getData.querySelector('img').alt = Anime.title;
+    getData.querySelector('img').src = jpg.image_url;
+    getData.querySelector('img').alt = title;
 
     getData.querySelector('h3').textContent = Anime.title;
 
@@ -55,5 +25,4 @@ fetch('https://api.jikan.moe/v4/top/anime')
     })
 }).catch((error) => {
     console.error('Error fetching data:', error);
-   
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html","desc.html"],
   theme: {
     extend: {
       colors: {
